2851e8c389a5610351e7f84eda1e48710f899cb2
Shubbham Jain <shubham.jain368@gmail.com>
2022-06-07 11:37:24 +0530

    fix: deployment bugs

--- base/models.py
+++ base/models.py
@@ -1,15 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
-class User(AbstractUser):
-    name = models.CharField(max_length=200, null=True)
-    email = models.EmailField(unique=True, null=True)
-    bio = models.TextField(null=True)
-    avatar = models.ImageField(null=True, default="avatar.svg")
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("User must have an email")
+        if not password:
+            raise ValueError("User must have a password")
+        if not username:
+            raise ValueError("User must have a full name")
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+        user = self.model(email=self.normalize_email(email))
+        user.username = username
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
     def create_superuser(self, email, username, password=None, **extra_fields):
         if not email:
@@ -24,6 +30,16 @@ class User(AbstractUser):
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+    avatar = models.ImageField(null=True, default="avatar.svg")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
 
 class Topic(models.Model):
